4c84d9b362b87a2805b1db44469b141053d9520e
rebtasa1988 <rebeca.talita@aluno.senac.rio>
2026-05-27 21:12:15 -0300

    ex15.py

---
+++ ex15.py
@@ -0,0 +1,7 @@
+temperatura=int(input("Qual a temperatura atual? "))
+if temperatura>=30:
+    print("Está muito quente!")
+elif temperatura <=18:
+    print("Está muito frio!")
+else:
+    print("A temperatura está agradável!")
--- ex17.py
+++
@@ -1,8 +0,0 @@
-temperatura = int(input("Qual a temperatura atual? "))
-
-if temperatura>=30:
-    print("Hoje o dia está quente!")
-elif temperatura<=18:
-    print("Hoje o dia está frio!")
-else:
-    print("A temperatura está agradável!")
